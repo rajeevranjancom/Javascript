b67d72a76e7eaa35a7b8fe9d6913a3fc541c2332
Rajeev Ranjan <50344183+rajeevranjancom@users.noreply.github.com>
2021-04-02 00:19:25 +0530

    new

---
+++ new-tab-page-1617281410033.js
@@ -0,0 +1,258 @@
+
+console.log(color);
+console.log(color[1]);
+console.log(color[0]);
+VM55:2 (2) ["red", "Green"]
+VM55:3 Green
+VM55:4 red
+undefined
+let color = ['red', 'Green'];
+console.log(color);
+console.log(color[1]);
+// console.log(color[0]);
+VM64:2 (2) ["red", "Green"]
+VM64:3 Green
+undefined
+let color = ['red', 'Green'];
+console.log(color);
+console.log(color[1]);
+console.log(color[0]);
+VM69:2 (2) ["red", "Green"]0: "red"1: "Green"length: 2__proto__: Array(0)
+VM69:3 Green
+VM69:4 red
+undefined
+typeof color
+"object"
+let color = ['red', 'Green'];
+console.log(color);
+console.log(color[1]);
+console.log(color[0]);
+VM96:2 (2) ["red", "Green"]
+VM96:3 Green
+VM96:4 red
+undefined
+typeof color;
+"object"
+function greet (){
+    console.log('Hello')
+}
+undefined
+function greet (){
+    console.log('Hello')
+}
+
+greet();
+VM132:2 Hello
+undefined
+function greet (name){
+    console.log('Hello'+" "+ name)
+}
+
+greet('Rabindra');
+VM138:2 Hello Rabindra
+undefined
+function name (firstName, lastName){
+    console.log('Welcome'+" "+firstName+" "+lastName);
+};
+
+name('Rajeev', 'Ranjan');
+VM144:2 Welcome Rajeev Ranjan
+undefined
+function square (number){
+    return number * number;
+}
+
+let number = square(20);
+console.log(number);
+VM150:6 400
+undefined
+let x = 10;
+let y = 20;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+console.log(x/y);
+
+VM156:4 30
+VM156:6 -10
+VM156:8 200
+VM156:10 0.5
+VM156:12 0.5
+undefined
+let x = 10;
+let y = 20;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+console.log(x%y);
+
+VM161:4 30
+VM161:6 -10
+VM161:8 200
+VM161:10 0.5
+VM161:12 10
+undefined
+typeof(x);
+"number"
+let x = 10;
+let y = 20;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+console.log(x%y);
+typeof(x);
+
+VM172:4 30
+VM172:6 -10
+VM172:8 200
+VM172:10 0.5
+VM172:12 10
+"number"
+let x = 10;
+console.log(x);
+console.log(++x);
+console.log(x++);
+console.log(x);
+VM178:2 10
+VM178:3 11
+VM178:4 11
+VM178:5 12
+undefined
+let x = 10;
+console.log(x);
+console.log(++x);
+console.log(x++);
+console.log(x);
+VM183:2 10
+VM183:3 11
+VM183:4 11
+VM183:5 12
+undefined
+
+
+
+// let name;
+// let age;
+
+let person = {
+    name : 'mosh',
+    age : 25
+    
+};
+console.log(person);
+
+
+ let person = {
+name : 'mosh',
+age : 28,
+};
+
+person.name = 'john';
+console.log(person.name);
+
+
+let color = ['red', 'Green'];
+console.log(color);
+console.log(color[1]);
+console.log(color[0]);
+
+
+function greet (){
+    console.log('Hello')
+}
+
+greet();
+
+function greet (name){
+    console.log('Hello'+" "+ name)
+}
+
+greet('Rabindra');
+
+function name (firstName, lastName){
+    console.log('Welcome'+" "+firstName+" "+lastName);
+};
+
+name('Rajeev', 'Ranjan');
+
+
+function square (number){
+    return number * number;
+}
+
+let number = square(20);
+console.log(number);
+
+let x = 10;
+let y = 20;
+
+console.log(x+y);
+
+console.log(x-y);
+
+console.log(x*y);
+
+console.log(x/y);
+
+console.log(x%y);
+typeof(x);
+
+
+let x = 10;
+console.log(x);
+console.log(++x);
+console.log(x++);
+console.log(x);
+
+
+// let name;
+// let age;
+
+let person = {
+    name : 'mosh',
+    age : 25
+    
+};
+console.log(person);
+
+
+ let person = {
+name : 'mosh',
+age : 28,
+};
+
+person.name = 'john';
+console.log(person.name);
+
+
+let color = ['red', 'Green'];
+console.log(color);
+console.log(color[1]);
+console.log(color[0]);
+
+let x = 10;
+console.log(x);
+console.log(--x);
+console.log(x--);
+console.log(x);
+
+
+
