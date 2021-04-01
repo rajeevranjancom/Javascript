acd70701c14a739d7a45cf59f6d6a98d9815e293
Rajeev Ranjan <50344183+rajeevranjancom@users.noreply.github.com>
2021-04-02 00:19:53 +0530

    Day80

---
+++ Day80.js
@@ -0,0 +1,49 @@
+*****************Variable**********************
+
+let firstName = 'Rajeev Ranjan';
+let lastName = "Yadav"
+console.log(firstName+" "+lastName);
+
+****************notes about Variable***********
+
+1) can't be a reserved Keyword
+2) Should be meaningful
+3) can't start with a number
+4) can't contain a space or hyphen
+5) Are case sensitive
+
+********************Constants******************
+
+const intersetRate = 10.3;
+console.log(intersetRate);
+
+-> used for constant data once decleared can't change
+
+
+*********************Primitive Types******************
+
+let name = 'Rajeev';
+let age = 35
+let isApproved = false;
+let firstName = undefined;
+let selectColor = null;
+
+console.log(name+" "+age+" "+isApproved+" "+firstName+" "+selectColor);
+
+let name = 'Raje';
+undefined
+typeof name;
+"string"
+let age = 20;
+undefined
+
+>>>>>>>>>Primitive / value Type >>>>>>>>>>>>>>>>>
+
+string 
+number
+Boolean
+null
+undefined
+
+
+
