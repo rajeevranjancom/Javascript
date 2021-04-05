2c9886f2bd9f0046cb74ce5814240ed9338a25d3
Rajeev Ranjan <50344183+rajeevranjancom@users.noreply.github.com>
2021-04-06 00:43:57 +0530

    Basicc

---
+++ Basicc/script.js
@@ -0,0 +1,61 @@
+// let val;
+// val = String(555);
+// val = 4 + 4
+// val = String(4 + 4);
+// val = String(true);
+// val = String(new Date());
+// val = String([1, 2, 3, 4, 5]);
+// val = (5).toString();
+// val = (true).toString();
+// console.log(val);
+// val = Number('5');
+// val = Number(true);
+// val = Number(false);
+// val = Number(null);
+// val = Number('hello');
+// val = Number([1, 2, 3, 4, 5]);
+// val = parseInt(5);
+// val = parseInt(5.5);
+// val = parseFloat(5.301);
+// console.log(val);
+// const val1 = 5;
+// const val2 = 10;
+// const sum = val1 + val2;
+// console.log(val1 + val2);
+// console.log(sum);
+// console.log(typeof (sum));
+
+
+// <----------------- Number and the math object----------->
+
+// const nums1 = 101;
+// const nums2 = 10;
+
+// console.log(nums1 + nums2);
+// console.log(nums1 - nums2);
+// console.log(nums1 * nums2);
+// console.log(nums1 / nums2);
+// console.log(nums1 % nums2);
+
+// <---------------------Math Object ------------------------>
+
+// let val;
+
+// val = Math.PI;
+// val = Math.E;
+// val = Math.ceil(5.6);
+// val = Math.floor(5.6);
+// val = Math.round(5.4); // this will choose 5
+// val = Math.round(5.6); // this will choose 6
+
+// val = Math.sqrt(64);
+// val = Math.abs(-3);
+// val = Math.pow(8, 2);
+// val = Math.min(2, 33, 4, 1, 55, 6, 3, -2);
+// val = Math.max(2, 33, 4, 1, 55, 6, 3, -2);
+// console.log(val);
+
+// val = Math.random() + 1;
+// val = Math.floor(Math.random() * 6 + 1);
+// console.log(val);
+
