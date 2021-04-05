8db65bb45a2f65aff38626e5a4990b957acb1129
Rajeev Ranjan <50344183+rajeevranjancom@users.noreply.github.com>
2021-04-05 07:57:22 +0530

    script

---
+++ DOM/script.js
@@ -0,0 +1,21 @@
+// let val;
+// val = document;
+// val = document.all;
+// val = document.all[1];
+// val = document.all.length;
+// val = document.head;
+// val = document.body;
+// val = document.doctype;
+// val = document.domain;
+// val = document.URL;
+// val = document.characterSet;
+// val = document.contentType;
+// val = document.forms;
+// val = document.forms[0];
+// val = document.forms[0].id;
+// val = document.forms[0].id.method;
+// val = document.forms[0].action;
+// val = document.link;
+// val = document.link[0];
+
+// console.log(val);
