2317ee682260a71cea6dcf9330bfedaac51c5088
Rajeev Ranjan <50344183+rajeevranjancom@users.noreply.github.com>
2021-04-05 07:50:52 +0530

    script

---
+++ Basic/script.js
@@ -0,0 +1,119 @@
+// console.log("Hello world");
+// console.log(123);
+// console.log(true);
+
+// var greeting = 'Hello';
+// console.log(greeting);
+// console.log([1, 2, 3, 4]);
+
+// console.log({ a: 1, b: 2 });
+// console.table({ a: 1, b: 2 });
+
+// console.error({ a: 1, b: 2 });
+// console.log("This is an error");
+// console.warn("u man:(");
+
+// console.time('Hello');
+// console.time('Hello');
+// console.time('Hello');
+// console.time('Hello');
+// console.time('Hello');
+// console.time('Hello');
+// console.time('Hello');
+// console.time('Hello');
+// console.time('Hello');
+// console.time('Hello');
+// console.timeEnd('Hello');
+
+// let name = 'Pollax';
+// console.log(name);
+// name = 'smith Doe';
+// console.log(name);
+
+/* This is
+Init var
+*/
+
+// let greeting;
+// console.log(greeting);
+// greeting = 'Hello';
+// console.log(greeting);
+// console.log(typeof (name));
+
+
+// LET
+
+// let name;
+// name = 'pollax john';
+// console.log(name);
+// name = 'steve smith';
+// console.log(name);
+
+// CONST
+
+// const name = 'john';
+// const name = 'Pollax'; // will throw an error
+// console.log(name);
+
+
+// const person = {
+//     name: 'pollax',
+//     age: 23
+// }
+
+// person.name = 'nancy';
+// person.age = 24
+
+// console.log(person);
+
+// console.log(person.name);
+// console.log(person.age);
+
+// const number = [1, 2, 3, 4, 5];
+// console.log(number);
+// number.push(6);
+// console.log(number);
+// number.pop(2);
+// console.log(number);
+
+// const name = 'Pollax john';
+// const age = 23;
+// const hasChild = true;
+// const car = null;
+// console.log("name" + " " + "age" + " " + hasChild + " " + car);
+
+// const sys = Symbol;
+// console.log(sys);
+// const hobbies = ['buddy', 'baby'];
+// console.log(hobbies);
+// console.log(hobbies[0]);
+
+// const address = {
+//     city: 'boston',
+//     state: 'MA'
+
+// }
+
+// console.log(address);
+
+// const today = new Date();
+// console.log(today);
+// console.log(typeof (today));
+
+// <-------------------------Type of Conversion----------->
+
+// let val;
+
+// val = String(555);
+// val = String(4 + 4);
+// val = String(true);
+
+// val = String(new Date());
+// console.log(typeof (val));
+
+// console.log(val);
+
+// val = String([1, 2, 3, 4, 5]);
+// val = (5).toString();
+// val = (true).toString();
+// console.log(val);
