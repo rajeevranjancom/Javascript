1c1bb1775fff41cd0f2386cd57a48fb0678eb84f
Rajeev Ranjan <50344183+rajeevranjancom@users.noreply.github.com>
2021-04-06 00:45:22 +0530

    Number

---
+++ Number/script.js
@@ -0,0 +1,46 @@
+// let firstName = 'Shankar Yadav';
+// let secondName = 'Shigh';
+// console.log(firstName + " " + secondName); //conct
+// console.log(typeof (firstName));
+
+// let DeepakMalev;
+// DeepakMale = false;
+// console.log(DeepakMale);
+// console.log(typeof (DeepakMale));
+
+
+// let name;
+// name = 'undefined';
+// console.log(typeof (name));
+
+// let pagla = null;
+// console.log(pagla);
+
+// let val;
+// val = 'Mohan'
+// console.log(val);
+
+// let Pollax;
+// let Pollaxpagla;
+// Pollax = Math.sqrt(64);
+// console.log(Pollax);
+// console.log(typeof (Pollax));
+
+// Pollax = Math.ceil(5.2);
+// Pollaxpagla = Math.floor(5.2);
+// let m;
+// let Mohan;
+// let M2;
+// Mohan = Math.max(12, 5, 10, 1, 2, 9, 50);
+// M2 = Math.min(12, 5, 10, 1, 2, 9, 50);
+// console.log("biggest number is" + " " + Mohan);
+// console.log("smallest number is" + " " + M2);
+
+// let Shankar;
+// Shankar = Math.pow(8, 3);
+// console.log(Shankar);
+
+
+// let m;
+// m = Math.floor(Math.random() * 6 + 1);
+// console.log(m);
