145ec7a206ddca376a04bc06d2db700d6a5a667c
Rajeev Ranjan <50344183+rajeevranjancom@users.noreply.github.com>
2021-04-07 00:08:32 +0530

    New

---
+++ New/script.js
@@ -0,0 +1,129 @@
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// console.log(0);
+// console.log(1);
+// console.log(2);                  //for Loop
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+
+// for (let i = 1; i <= 20; i++) {
+//     if (i % 2 == 0) {
+//         console.log('divided by 2');
+//     }
+//     else {
+//         console.log('not divided by 2')
+//     }
+// }
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+
+// }
+// i++;
+
+// let i = 0;
+// while (i <= 20) {
+//     if (i % 2 == 0) {
+//         console.log('Divided by 2');
+//     }
+
+//     else {
+//         console.log('not divided by 2');
+//     }
+
+// }
+// let val;
+// val = Math.PI = 4.22;
+// console.log(val);
+
+// console.table({
+//     a: 'Rajeev', b: 20, c: 10, d: 20, e: 10, f: 20, g: 10, h: 20, i: 10, j: 20,
+//     k: 10, l: 20, m: 10, n: "Shankar"
+// });
+
+// let person = {
+//     name: 'Pollax',
+//     age: 23,
+// }
+
+// console.log(person);
+
+
+// let person = {
+//     name: 'Pollax',
+//     age: 23,
+// }
+
+// console.log(person.name);
+// console.log(person.age);
+
+// let color = ['Green', 'Blue'];
+// console.log(color);
+// console.log(color.length);
+// console.log(color[0][3] + " " + color[1]);
+
+
+// function greeting() {
+//     console.log('Hello :)');
+// }
+// greeting();
+
+// function greeting(name) {
+//     console.log('Hello :)' + " " + name);
+// }
+// greeting('pollax');
+
+// function name(firstName, lastName) {
+//     console.log('hola' + " " + firstName + " " + lastName);
+
+// }
+
+// name('pollax', 'john');
+
+
+// function sq(num) {
+//     return num * num;
+// }
+// console.log(sq(5));
+
+// function sq(num) {
+//     console.log(num * num);
+// }
+
+// sq(10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
