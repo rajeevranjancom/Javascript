c262869a68597fe15ffba139e7ed4032cea21bfb
Rajeev Ranjan <50344183+rajeevranjancom@users.noreply.github.com>
2021-04-07 06:54:49 +0530

    Day01

---
+++ Day01/script.js
@@ -0,0 +1,218 @@
+// function greet() {
+//     console.log('Hello');
+
+// };
+
+// greet();
+
+// function greet(name) {
+//     console.log(greet);
+// };
+
+// greet('Pollax');
+
+// function greet(firstName, lastName) {
+//     console.log(firstName + " " + lastName);
+
+// };
+
+// greet('john', 'pollax');
+
+//operator----->
+
+// let x = 10;
+// let y = 3;
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+// increament ------>
+
+// let x = 10;
+// console.log(x++);
+// console.log(x++);
+// console.log(x);
+
+// console.log(x--);
+// console.log(x--);
+// console.log(x--);
+
+// x = x + 5;
+// x += 5;
+// console.log(x);
+
+// comparision ------->
+
+// let x = 1;
+
+// console.log(x > 0);
+// console.log(x >= 1);
+// console.log(x < 1);
+// console.log(x != 1);
+// console.log(x == 1);
+
+// Equality-------------->
+//strct Equality
+
+// console.log(1 === 1);
+// console.log('1' === 1);
+
+// loosly Equality------->
+
+// console.log(1 == 1);
+// console.log('1' == 1);
+// console.log(true == 1);
+
+// ternory operator ------>
+
+// let point = 10;
+// let type = point > 100 ? 'gold' : 'silver';
+// console.log(type)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//          NEXT PART 
+
+// Basic part
+
+// console.log('hola');
+// console.log(123);
+// console.log('hola');
+// let Greeting = 'king';
+// console.log(Greeting);
+// console.table({ a: 1, b: 2 });
+// console.log({ a: 1, b: 2 });
+// console.error('this is error');
+
+// console.warn('warnig');
+// console.time('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.log('hello');
+// console.timeEnd('hello')
+
+// var name = 'joker!'
+// console.log(name);
+
+// var greeting;
+// greeting = 'joker'
+// console.log(greeting);
+
+
+// let name;
+// name = 'killer';
+// console.log(name);
+// name = 'Dead';
+// console.log(name);
+
+// const name = 'holla';
+// console.log(name);
+// const name = 'jain';  // this will throw an error
+// console.log(name);
+
+// const person = {
+//     name: 'pollax',
+//     age: 22
+// };
+
+// console.log(person);
+// console.log(person.name);
+// console.log(person.age);
+
+// const number = [1, 2, 3, 4, 5];
+// console.log(number);
+// console.log(number.length);
+// console.log(number[0, 3]);
+// console.log(number[6]);
+
+// number.push(6);
+// console.log(number);
+// number.push(1);
+// number.pop(1);
+// number.pop([2])
+// console.log(number);
+
+// data types(primitive type)
+
+// let name = 'jocker';
+// console.log(name); //string
+
+// let age = 45;
+// console.log(age); //number
+// console.log(typeof (age));
+
+// const isChild = false;
+// console.log(isChild); //Boolean
+
+// const car = null;
+// console.log(car);
+
+// const Bike = undefined; //undefined
+// console.log(Bike);
+
+// const sys = symbol();
+// console.log(sys);
+
+
+// Reference types (Data types)
+
+// const address = {
+//     city: 'Boston',
+//     state: 'miami'
+
+// }
+
+// console.log(address);
+
+// console.log(address.city);
+// console.log(address.state);
+
+
+// const today = new Date();
+// console.log(today);
+// console.log(typeof (today));
+
+
+// Type conservation
+
+// let val;
+// val = String(555);
+// val = 4 + 4;
+// val = String(4 + 4);
+// console.log(val);
+
+
+// date to string
+
+// val = String(new Date());
+// val = new Date();
+// console.log(val);
+
+
